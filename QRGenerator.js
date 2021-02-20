f7c9c87abf0a212c0c4b3abe5a0ffd6a379c2e09
eai-oen <nicholaswnzhang@gmail.com>
2021-02-20 13:04:06 -0800

    Add QRGenerator library

--- QRGenerator.js
+++ QRGenerator.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
 
 export default function SenderScreen(props) {
   return (
     <View>
       <Text>Sender Screen</Text>
+      <QRCode
+        value="Minato Aqua"
+      />
     </View>
   )
 }
