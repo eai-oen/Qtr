852f0d3d3aa160b772a53c2cd501e262d610784b
eai-oen <nicholaswnzhang@gmail.com>
2021-02-20 15:27:58 -0800

    Test FileSystem

--- QRGenerator.js
+++ QRGenerator.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Platform, Button, StyleSheet, Text, View } from 'react-native';
 import QRCode from 'react-native-qrcode-svg';
 import * as DocumentPicker from 'expo-document-picker';
 import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
+
 //import DocumentPicker from 'react-native-document-picker';
 
 export default class SenderScreen extends React.Component {
   state = {
-    data: null
+    data: null,
+    base64: "fuck",
+    status: "choose",
   }
   async getFile() {
     const result = await DocumentPicker.getDocumentAsync();
@@ -28,9 +32,42 @@ export default class SenderScreen extends React.Component {
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
+      base64: true,
     });
 
     this.setState({data: result});  
+    console.log(result.base64.substring(0, 100));
+    this.setState({status: "loading"});
+    const boi = await FileSystem.readDirectoryAsync(FileSystem.documentDirectory);
+    console.log(boi);
+
+    const extension = result.uri.substring(result.uri.lastIndexOf('.'));
+    await FileSystem.writeAsStringAsync(FileSystem.documentDirectory + "myfile" + extension, 'data:image/png;base64,' + result.base64);
+    //const contents = await FileSystem.readAsStringAsync(result.uri, {encoding: FileSystem.EncodingType.Base64})
+    
+
+    // You can also display the image using data:
+    // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+    // const extension = result.uri.substring(result.uri.lastIndexOf('.'));
+    // const imagePath = `${RNFS.DocumentDirectoryPath}/${new Date().toISOString()}`.replace(/:/g, '-') + extension;
+
+    // if(Platform.OS === 'ios') {
+    //   RNFS.copyAssetsFileIOS(result.uri, imagePath, 0, 0)
+    //     .then(res => {
+    //       console.log("successs");
+    //     })
+    //     .catch(err => {
+    //       console.log('ERROR: image file write failed!!!');
+    //       console.log(err.message, err.code);
+    //     });
+    // } 
+    this.setState({status: "loaded"});
+
+    // console.log(contents);
+    // console.log("my dick");
+    
+    // console.log(extension);
+    //await FileSystem.writeAsStringAsync(FileSystem.documentDirectory + "myfile" + extension, 'data:image/jpg;base64,' + contents);
   }
 
   render() {
@@ -42,10 +79,15 @@ export default class SenderScreen extends React.Component {
           size={300}
         />
         <Button
-          title="Get File"
+          title="Get Image"
           onPress={() => this.getImage()}
         />
-        <Text>{this.state.data ? this.state.data.size + " " + this.state.data.uri : "Lmao" }</Text>
+        <Button
+          title="Get File"
+          onPress={() => this.getFile()}
+        />
+        <Text>{"Status: " + this.state.status}</Text>
+        <Text>{this.state.data ? this.state.data.width + " " + this.state.data.height + " " + this.state.data.uri + " " + this.state.base64: "Lmao" }</Text>
       </View>
     );
   }
